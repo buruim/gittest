9e7d8172bef8af5051edee3e94f8591c1890a9e2
buruim <sjqnfnla@gmail.com>
2024-01-28 21:22:40 +0900

    main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import addition
+import subtraction
+import multiplication
+import division
+import modulus
+
+# 입력
+a, b = map(int, input.split())
+
+# 출력
+print()
+print()
+print()
+print()
+print()
+
