bc0060539f98892e7802c28efcadbaa05f458e12
buruim <sjqnfnla@gmail.com>
2024-01-28 21:45:24 +0900

    modify main

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-import addition
-import subtraction
-import multiplication
-import division
-import modulus
+from addition import add
+from division import div
 
 # 입력
 a, b = map(int, input.split())
 
 # 출력
-print()
-print()
-print()
 print()
 print()
 
